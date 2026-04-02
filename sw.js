c1df9497236e8ac08459dc20cc6abf56c07c2638
augustoflotteron <ffeliteapartments@gmail.com>
2026-04-02 21:24:40 +0200

    🔄 Aggiorna cache SW [auto]

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // NOTE: Update CACHE_VERSION manually on every deploy so users receive fresh assets.
-const CACHE_VERSION = '2026-04-02-v1775147703982';
+const CACHE_VERSION = '2026-04-02-v1775157880350';
 const CACHE_NAME = 'bnb-guide-' + CACHE_VERSION;
 const STATIC_ASSETS = [
   './',
